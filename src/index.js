1cda3535387dd1bb70a6db8e4bbd748772a76b57
George McCarron <hello@georgemccarron.com>
2021-03-21 20:19:44 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./Components/MainApp/App.js";
 import reportWebVitals from "./reportWebVitals";
 
+const unused = 'blah';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
